7e4435b0cbe5f5edeccfeb7923b3f9ae81ba50a3
Marvin Menzerath <menzerath@ihf.rwth-aachen.de>
2017-03-15 09:27:42 +0100

    Include more CORS headers in update.php

--- update.php
+++ update.php
@@ -1,6 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
 if (!isset($_GET['core']) || empty($_GET['core'])) {
 	$givenCoreVersion = '0.0.0';
